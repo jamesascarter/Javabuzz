040f55527745857f5722a09015871c8c87e3a2ea
jamesascarter <jamesascarter@gmail.com>
2014-10-14 12:21:57 +0100

    Added return strings. All tested. Exercise complete

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -38,5 +38,24 @@ describe('Javabuzz', function() {
 
 	});
 
+	describe('when playing, says', function() {
+
+		it('"Java" when a  number is divisible by 3', function() {
+			expect(javabuzz.says(3)).toEqual("Java");
+		});
+
+		it('"Buzz" when a  number is divisible by 5', function() {
+			expect(javabuzz.says(5)).toEqual("Buzz");
+		});
+
+		it('"JavaBuzz" when a  number is divisible by 15', function() {
+			expect(javabuzz.says(15)).toEqual("JavaBuzz");
+		});
+
+		it('"number" when a  number is not divisible by 3 or 5', function() {
+			expect(javabuzz.says(2)).toEqual(2);
+		});
+	});
+
 });
 
